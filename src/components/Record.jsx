cd84d8743aba38caa59526b89230d8e954757bb7
sk Valipasha <saisk9123@gmail.com>
2025-02-06 08:04:30 +0530

    updated-dockerfile-dev-build-pipe-only

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import config from "../../config"; // Import the configuration
 
 export default function Record() {
   const [form, setForm] = useState({
@@ -17,7 +18,7 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/record/${params.id.toString()}`
+        `${config.API_BASE_URL}/record/${params.id.toString()}` // Use the API_BASE_URL from config
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +52,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/record`, {
+        response = await fetch(`${config.API_BASE_URL}/record`, { // Use the API_BASE_URL from config
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +61,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/record/${params.id}`, {
+        response = await fetch(`${config.API_BASE_URL}/record/${params.id}`, { // Use the API_BASE_URL from config
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
